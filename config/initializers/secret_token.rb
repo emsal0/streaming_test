ca49908e383a382752b10bd8c13726843ccee57e
az <absolutezero2a03@gmail.com>
2014-04-03 21:08:33 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StreamingTest::Application.config.secret_key_base = '2917e2c986089f49cdad299543b37b2b128ddd6d9d3585d8517ec0b95e417cbf45a8f82957102465c949162ad337144ba8bdc456cd635cc5be07578c99917eee'
